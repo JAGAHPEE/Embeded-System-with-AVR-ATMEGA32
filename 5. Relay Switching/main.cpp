35f6c7ec91b3f98efed956793996c460f3bab998
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-08-11 20:35:07 +0530

    Add files via upload

--- 5. Relay Switching/main.cpp
+++ 5. Relay Switching/main.cpp
@@ -1,1 +1,25 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 30-07-2023 10:12:07
+ * Author : kales
+ */ 
+
+#include <avr/io.h>
+#define  F_CPU 16000000L
+#include <util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRC |=(1<<7);
+	
+    while (1) 
+    {
+		PORTC |=(1<<7);
+		_delay_ms(1000);
+		PORTC &=~(1<<7);
+		_delay_ms(1000);
+    }
+}
 
