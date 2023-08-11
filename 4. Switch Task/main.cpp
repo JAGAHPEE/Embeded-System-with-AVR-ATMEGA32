7a60bcc40c34d5196f5c92bfa0edff065b79a7e4
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-08-11 20:34:11 +0530

    Add files via upload

--- 4. Switch Task/main.cpp
+++ 4. Switch Task/main.cpp
@@ -1,1 +1,46 @@
+/*
+ * Switch_Task.cpp
+ *
+ * Created: 29-07-2023 18:49:39
+ * Author : kales
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000L
+#include <util/delay.h>
+
+int main(void)
+{
+    // To make PB1 and PB5 as Input
+	DDRB &=~(1<<1);
+	DDRB &=~(1<<5);
+	//To enable both pin Pull up resistors.
+	PORTB |=(1<<1);
+	PORTB |=(1<<5);
+	//To define pin for led -> PD1 and PD5
+	DDRD |=(1<<1);
+	DDRD |=(1<<5);
+	
+    while (1) 
+    {
+		//if condition for led toggle
+	//	if((PINB & 0x08)==0) //0000 1000
+		if((PINB & 0x02)==0) //0000 0010
+		{
+			PORTD |=(1<<1);
+		}
+		else
+		{
+			PORTD &=~(1<<1);
+		}
+		if((PINB & 0x20)==0) //0010 000
+		{
+			PORTD |=(1<<5);
+		}
+		else
+		{
+			PORTD &=~(1<<5);
+		}
+    }
+}
 
