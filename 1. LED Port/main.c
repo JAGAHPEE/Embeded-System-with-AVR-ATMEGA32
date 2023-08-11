aea01a09c91a2e51781ac2d40073574df8b9c107
Sandesh Kale <87817858+JAGAHPEE@users.noreply.github.com>
2023-08-11 20:30:11 +0530

    Add files via upload

--- 1. LED Port/main.c
+++ 1. LED Port/main.c
@@ -1,1 +1,22 @@
+/*
+ * main.c
+ *
+ * Created: 7/29/2023 9:10:24 AM
+ *  Author: kales
+ */ 
 
+#include <xc.h>
+#include <avr/io.h>
+#define F_CPU 16000000L
+#include <util/delay.h>
+int main(void)
+{
+	DDRB=0xff;
+    while(1)
+    {
+        PORTB = 0xff;
+		_delay_ms(1000);
+		PORTB = 0x00;
+		_delay_ms(1000);
+    }
+}
